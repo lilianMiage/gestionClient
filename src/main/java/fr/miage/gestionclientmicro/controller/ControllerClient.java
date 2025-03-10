100f5b6f58eba7bbb1042114e4aa8055fff4c9c9
Lilianroux <lilianroux02@gmail.com>
2025-03-10 15:40:29 +0100

    Create service

--- src/main/java/fr/miage/gestionclientmicro/controller/ControllerClient.java
+++ src/main/java/fr/miage/gestionclientmicro/controller/ControllerClient.java
@@ -1,4 +1,18 @@
 package fr.miage.gestionclientmicro.controller;
 
+import fr.miage.gestionclientmicro.service.ServiceClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Provider;
+
+@RestController
+@RequestMapping("/api/client")
 public class ControllerClient {
+
+    @Autowired
+    private ServiceClient serviceClient;
+
+
 }
--- src/main/java/fr/miage/gestionclientmicro/service/ServiceClient.java
+++ src/main/java/fr/miage/gestionclientmicro/service/ServiceClient.java
@@ -1,4 +1,27 @@
 package fr.miage.gestionclientmicro.service;
 
+import fr.miage.gestionclientmicro.entities.Client;
+import fr.miage.gestionclientmicro.repo.RepoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ServiceClient {
+
+    @Autowired
+    private RepoClient repoClient;
+
+    public void createClient(Client client){
+        repoClient.save(client);
+    }
+
+    public Client getClientById(String id) throws Exception {
+        Optional<Client> client = repoClient.findById(id);
+        if(client.isEmpty()){
+            throw new Exception("Client doesn't exist");
+        }
+        return client.get();
+    }
 }
